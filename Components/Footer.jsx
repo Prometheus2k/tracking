4d7831dbc0eadc5333224c2ca1c2fe8202704a86
Prometheus2k <shonesk2000@gmail.com>
2023-05-29 11:29:52 +0530

    Added Footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { Fot1, Fot2 } from "@/Components/index";
 
-const Footer = () => {
-  return <div>Footer</div>;
+export default () => {
+  const footerNavs = [
+    {
+      href: "javascript:void(0)",
+      name: "Terms",
+    },
+    {
+      href: "javascript:void(0)",
+      name: "License",
+    },
+    {
+      href: "javascript:void(0)",
+      name: "Privacy",
+    },
+    {
+      href: "javascript:void(0)",
+      name: "About us",
+    },
+  ];
+  return (
+    <footer className="pt-10">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+        <div className="justify-between sm:flex">
+          <div className="space-y-6">
+            <img src="https://www.floatui.com/logo.svg" className="w-32" />
+            <p className="max-w-md">
+              Link your supply chain with blockchain. The secure supply chain of
+              the future lies here.
+            </p>
+            <ul className="flex flex-wrap items-center gap-4 text-sm sm:text-base">
+              {footerNavs.map((item, idx) => (
+                <li className="text-gray-800 hover:text-gray-500 duration-150">
+                  <a key={idx} href={item.href}>
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="mt-6">
+            <p className="text-gray-700 font-semibold">Get the app</p>
+            <div className="flex items-center gap-3 mt-3 sm:block">
+              <a href="javascript:void(0)">
+                <Fot1 />
+              </a>
+              <a href="javascript:void(0)" className="mt-0 block sm:mt-3">
+                <Fot2 />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 py-10 border-t md:text-center">
+          <p>© 2023 Shone Stalin. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
-
-export default Footer;
